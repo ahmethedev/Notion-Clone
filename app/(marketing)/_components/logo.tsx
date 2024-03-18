b5f22a722427f769621350b3cc324c280e68ff6b
ahmethedev <ahmethedev@gmail.com>
2024-03-19 02:33:22 +0300

    Landing Page

---
+++ app/(marketing)/_components/logo.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["400", "600"]
+});
+
+export const Logo = () => {
+    return(
+        <div className="hidden md:flex items-center gap-x-2">
+            <Image src="/logo-2.svg" height="40" width="40" alt="Logo"
+            className="dark:hidden"></Image>
+            <Image src="/logo-dark.svg" height="40" width="40" alt="Logo"
+            className="hidden dark:block"></Image>
+            <p className={cn("font-semibold", font.className)}>Jotion</p>
+        </div>
+    )
+}
---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Jotion",
+  description: "Better than Notion",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/logo-2.svg",
+        href: "/logo-2.svg",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo-dark.svg",
+        href: "/logo-dark.svg",
+      }
+    ]
+  }
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+        storageKey="jotion-theme">
+        {children}
+        </ThemeProvider>
+        </body>
+
+    </html>
+  );
+}
